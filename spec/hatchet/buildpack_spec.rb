9a907d01c67fb6cd3e159146e4f17eabf8c7eae9
schneems <richard.schneeman+foo@gmail.com>
2020-06-26 09:59:57 -0500

    First

--- spec/hatchet/buildpack_spec.rb
+++ spec/hatchet/buildpack_spec.rb
@@ -1,103 +1,41 @@
 require_relative "../spec_helper.rb"
 
 RSpec.describe "This buildpack" do
-  it "accepts absolute paths at build and runtime" do
+  it "sets env vars that other buildpacks pick up" do
     app_dir = generate_fixture_app(
-      name: "works",
+      name: "echos buildpack_vendor_url",
       compile_script: <<~EOM
         #!/usr/bin/env bash
 
-
-        # Test export
-        echo "export PATH=/good/absolute/path:$PATH" >> export
-
-        # Test .profile.d
-        BUILD_DIR=$1
-        mkdir -p $BUILD_DIR/.profile.d
-
-        echo "export PATH=/good/absolute/path:$PATH" >> $BUILD_DIR/.profile.d/my.sh
+        echo "BUILDPACK_VENDOR_URL is set to $BUILDPACK_VENDOR_URL"
       EOM
     )
 
     buildpacks = [
+      :default,
       "https://github.com/heroku/heroku-buildpack-inline",
-      :default
     ]
 
-    Hatchet::Runner.new(app_dir, buildpacks: buildpacks).deploy do |app|
-      expect(app.output).to_not include("All paths must be absolute")
+    Hatchet::Runner.new(app_dir, buildpacks: buildpacks, config: {"__SUDO_BUILDPACK_VENDOR_URL": "bloop"}).deploy do |app|
+      expect(app.output).to include("BUILDPACK_VENDOR_URL is set to bloop")
     end
   end
 
-  describe "export detection" do
-    it "errors on relative paths" do
-      app_dir = generate_fixture_app(
-        name: "export_fails_relative",
-        compile_script: <<~EOM
-          #!/usr/bin/env bash
+  it "fails if no 'sudo' env vars are set" do
+    app_dir = generate_fixture_app(
+      name: "empty",
+      compile_script: <<~EOM
+        #!/usr/bin/env bash
+      EOM
+    )
+    buildpacks = [
+      :default,
+      "https://github.com/heroku/heroku-buildpack-inline",
+    ]
 
-          echo "export PATH=bad_export_path_because_im_relative:$PATH" >> export
-        EOM
-      )
-
-      buildpacks = [
-        "https://github.com/heroku/heroku-buildpack-inline",
-        :default
-      ]
-
-      Hatchet::Runner.new(app_dir, buildpacks: buildpacks, allow_failure: true).deploy do |app|
-        expect(app.output).to include("All paths must be absolute")
-      end
-    end
-  end
-
-  describe "profile.d detection" do
-    it "errors on relative paths" do
-      app_dir = generate_fixture_app(
-        name: "export_fails_relative",
-        compile_script: <<~EOM
-          #!/usr/bin/env bash
-
-          BUILD_DIR=$1
-          mkdir -p $BUILD_DIR/.profile.d
-
-          echo "export PATH=bad_export_path_because_im_relative:$PATH" >> $BUILD_DIR/.profile.d/my.sh
-        EOM
-      )
-
-      buildpacks = [
-        "https://github.com/heroku/heroku-buildpack-inline",
-        :default
-      ]
-
-      Hatchet::Runner.new(app_dir, buildpacks: buildpacks, allow_failure: true).deploy do |app|
-        expect(app.output).to include("All paths must be absolute")
-      end
-    end
-  end
-
-  describe "leaky build path detection" do
-    it "errors on leaky paths" do
-      app_dir = generate_fixture_app(
-        name: "leaky_build_path",
-        compile_script: <<~EOM
-          #!/usr/bin/env bash
-
-          BUILD_DIR=$1
-          mkdir -p $BUILD_DIR/.profile.d
-
-          echo "export PATH=$BUILD_DIR/foo:$PATH" >> $BUILD_DIR/.profile.d/my.sh
-        EOM
-      )
-
-      buildpacks = [
-        "https://github.com/heroku/heroku-buildpack-inline",
-        :default
-      ]
-
-      Hatchet::Runner.new(app_dir, buildpacks: buildpacks, allow_failure: true).deploy do |app|
-        expect(app.output).to include("A build path leaked into runtime")
-      end
+    Hatchet::Runner.new(app_dir, buildpacks: buildpacks, allow_failure: true).deploy do |app|
+      expect(app.output).to include("No sudo env vars found")
+      expect(app.deployed?).to be_falsey
     end
   end
 end
